ab5a526ca230c1744a7de13dc33389dbdd17888b
Soyalkhan <soyalkhan76@gmail.com>
2024-08-28 01:56:00 +0530

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,3 +85,17 @@ const sendTokenResponse = (user, statusCode, res,message) => {
         Message: message
     });
 };
+
+
+// Logout user and clear cookie
+exports.logout = (req, res) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged out successfully',
+    });
+};

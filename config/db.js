54c15361bab38f861ae8f638b87916b861b4a6a6
Soyalkhan <soyalkhan76@gmail.com>
2024-08-28 00:52:26 +0530

    useNewUrlParser,useUnifiedTopology removed casue unwanted now

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,   // This ensures compatibility with MongoDB's connection string format
-            useUnifiedTopology: true // This option helps with handling MongoDB's new connection management engine
+            // useNewUrlParser: true,   // This ensures compatibility with MongoDB's connection string format
+           // useUnifiedTopology: true // This option helps with handling MongoDB's new connection management engine
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);

c336b8c24f51cc6583954f34488be56d68b98df1
Soyalkhan <soyalkhan76@gmail.com>
2024-08-28 02:36:13 +0530

    username-update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login,logout, getMe } = require('../controllers/authController');
+const { register, login,logout,updateName, getMe } = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();

70938c612678d42b9ede393c520f50dbe7c9b20f
Soyalkhan <soyalkhan76@gmail.com>
2024-09-11 17:00:42 +0530

    get user change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -20,14 +20,20 @@ exports.protect = async (req, res, next) => {
 
     
     if (!token) {
-        return res.status(401).json({ success: false, message: 'Not authorized to access this route 1' });
+        return res.status(401).json({ success: false, message: 'No token provided, not authorized to access this route' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
+        
+        if (!req.user) {
+            return res.status(404).json({ success: false, message: 'User not found with this ID' });
+        }
         next();
     } catch (err) {
-        return res.status(401).json({ success: false, message: 'Not authorized to access this route 2' });
+        
+        console.error('Token verification error:', err);
+        return res.status(401).json({ success: false, message: 'Token verification failed, not authorized' });
     }
 };

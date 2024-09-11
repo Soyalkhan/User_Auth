93c0888a848e779e8acfdcb4f4656f617c8d80f0
Soyalkhan <soyalkhan76@gmail.com>
2024-09-11 17:51:47 +0530

    get to post

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/verify', verifyOTP);
 router.post('/login', login);
-router.get('/getUserData', protect, getMe);
+router.post('/getUserData', protect, getMe);
 router.get('/logout', logout); 
 router.put('/addBio', protect, bio)
 router.post('/LoginByphone' ,loginByPhone)

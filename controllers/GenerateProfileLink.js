5d654b8418b77b1f17b990ea86c359f119843cc8
Soyalkhan <soyalkhan76@gmail.com>
2024-09-19 11:26:13 +0530

    bio added in public link

--- controllers/GenerateProfileLink.js
+++ controllers/GenerateProfileLink.js
@@ -4,24 +4,27 @@ const User = require('../models/userModel');
 
 // generate user pofile link
 
-exports.getPublicProfile = async ( req , res ) =>{
+exports.getPublicProfile = async (req, res) => {
 
-    try{
+    try {
 
         const { username } = req.params;
-
-        if (!username ) {
+        
+        if (!username) {
             return res.status(400).json({ success: false, message: 'Please provide a username' });
         }
-        const user = await User.findOne({ username}).select(' name username links SocialURLs');
-        if(!user){
-            res.status(404),json({ success: false, message: 'user not found'});
+        
+        const user = await User.findOne({ username }).select(' name username bio links SocialURLs');
+
+        if (!user) {
+            res.status(404).json({ success: false, message: 'user not found or add username first.' });
         }
-    
-        res.status(200).json({ success: true, message: 'user profile data fetched.' , user: user});
+
+        res.status(200).json({ success: true, message: 'user profile data fetched.', user: user });
     }
-    catch(error){
+    catch (err) {
+        console.log(err);
+        
         res.status(500).json({ success: false, message: 'An error occurred while retrieving the profile.' });
     }
-    }
-   
+}
